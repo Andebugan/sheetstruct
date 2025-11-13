a79762d3bd0c8af3d7a2f2aad117708f222beaf5
Nikita A. Abrosimov <abn@ht-lab.ru>
2025-11-13 21:13:41 +0300

    Структура для подключения к БД

--- api/go.mod
+++ api/go.mod
@@ -9,6 +9,12 @@ require (
 )
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+)
+
+require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/bytedance/sonic v1.14.0 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
@@ -31,6 +37,7 @@ require (
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/goccy/go-yaml v1.18.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/jackc/pgx/v5 v5.7.6
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- api/internal/db/db.go
+++ api/internal/db/db.go
@@ -1,1 +1,91 @@
 package db
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var Pool *pgxpool.Pool
+
+type Config struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+// Получение конфигурации подключения к БД
+func FromEnv() Config {
+	return Config{
+		Host:     getenvDefault("PG_HOST", "localhost"),
+		Port:     getenvDefault("PG_PORT", "5432"),
+		User:     getenvDefault("PG_USER", "postgres"),
+		Password: getenvDefault("PG_PASSWORD", ""),
+		Database: getenvDefault("PG_DB", "postgres"),
+		SSLMode:  getenvDefault("PG_SSLMODE", "disable"),
+	}
+}
+
+// Считывание настроек окружения для конфигурации
+func getenvDefault(key, def string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return def
+}
+
+// Открытие нового туннеля подклюния к БД
+func NewPool(ctx context.Context, cfg Config) (*pgxpool.Pool, error) {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		url.QueryEscape(cfg.User),
+		url.QueryEscape(cfg.Password),
+		cfg.Host,
+		cfg.Port,
+		cfg.Database,
+		cfg.SSLMode,
+	)
+
+	pgxCfg, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, fmt.Errorf("parse postgres config: %w", err)
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, pgxCfg)
+	if err != nil {
+		return nil, fmt.Errorf("open postgres pool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, fmt.Errorf("ping postgres: %w", err)
+	}
+
+	return pool, nil
+}
+
+// Инициализация подключения к БД
+func Init(ctx context.Context) error {
+	cfg := FromEnv()
+
+	pool, err := NewPool(ctx, cfg)
+	if err != nil {
+		return err
+	}
+
+	Pool = pool
+	return nil
+}
+
+// Закрытие туннеля подключения к БД
+func Close() {
+	if Pool != nil {
+		Pool.Close()
+	}
+}
